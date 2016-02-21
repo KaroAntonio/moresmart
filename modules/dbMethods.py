f367c6c353006faccc7e04c6a0e3fe2d9902a80d
Yi Qiao Wang <yiqiao.wang96@gmail.com>
2016-02-21 03:51:20 -0500

    finished database methods, for now

--- modules/dbMethods.py
+++ modules/dbMethods.py
@@ -1,7 +1,6 @@
 # Python module for database functions.
 import json
 import ast
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
@@ -15,7 +14,6 @@ def toJSON(string):
 #wrapper, json to string
 def toString(JSON):
     return json.dumps(JSON)
-
 
 #returns all courses as a dict, pass in the query
 def returnAllCourses(courses):
@@ -31,28 +29,36 @@ def returnAllCourses(courses):
         dict["course"] = dict["course"].sort()
     return json.dumps(dict)
 
+
+def queryResponse(query_classes):
+    parsed_query_classes = ast.literal_eval(query_classes)
+    query_id = queryCourse(parsed_query_classes)[0].id
+    price = parsed_query_classes["price"]
+    
+    query_tutor = {"id":query_id,"maxprice":price}
+    return queryTutor(query_tutor)
+
 #conditions should be a dict, no arrays inside, returns result.
 def queryCourse(query):
     # structure of query might change
     #right  now ,we treat query as a json/dict
-    school = query["school"]
-    course = query["course"]
+    school = query['school']
+    course = query['course']
 
     result = db.session.query(Subject).filter(Subject.course == course, Subject.school == school)
     return result
 #we need to process query after we receieve, and after we get id
 
+
 #query should be a dict containing price and id, no arrays, again.
 def queryTutor(query):
     id = query["id"]
-    minprice = query["minprice"]
     maxprice = query["maxprice"]
     result = db.session.query(User).filter(User.pricemin <= maxprice)
     finalResult = []
     for item in result:
         subjects = item.subjects
         subjectsDict = ast.literal_eval(subjects)
-        print (subjectsDict)
         courses = subjectsDict["subjects"]
         for i in courses:
             if str(i) ==  str(id):
