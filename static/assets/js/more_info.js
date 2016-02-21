462b1b2c5e5e14fa94c2266a933fb9d12dc6317f
KaroAntonio <karoantonio@gmail.com>
2016-02-20 21:49:03 -0500

    more dropdown

--- static/assets/js/more_info.js
+++ static/assets/js/more_info.js
@@ -2,6 +2,13 @@ $('#number_button').click(function(){
 	submit_number()
 });
 
+var options = $("#options");
+result = ['opt1','opt2']
+
+$.each(result, function() {
+    options.append($("<option />").val(this).text(this));
+});
+
 function submit_number() {
     console.log('submit_number',$('#input-box').val())
     var request = $.ajax({
